b960277b1abfaef5568960dfd0b834d2b70acc03
Mazen Kotb <email@mazenmc.io>
2016-04-19 07:00:51 -0700

    Dear Boet, fuck you. Thanks, Mazen

--- src/main/java/pw/mzn/youtubebot/YoutubeBot.kt
+++ src/main/java/pw/mzn/youtubebot/YoutubeBot.kt
@@ -12,11 +12,14 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.time.Period
 import java.util.*
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 import java.util.regex.Pattern
 import kotlin.properties.Delegates
 import kotlin.system.exitProcess
 
 class YoutubeBot(val key: String, val youtubeKey: String) {
+    val executor = Executors.newFixedThreadPool(2)
     val playlistRegex = Pattern.compile("^.*(youtu\\.be\\/|list=)([^#&?]*).*")
     val videoRegex = Pattern.compile("^(?:https?:\\/\\/)?(?:www\\.)?(?:youtube\\.com|youtu\\.be)\\/watch\\?v=([^&]+)")
     val executable = File("youtube-dl")
@@ -55,14 +58,6 @@ class YoutubeBot(val key: String, val youtubeKey: String) {
         println("Finished downloading youtube-dl executable")
     }
 
-    private fun addSplit(list: List<Any>, splitter: String): String {
-        var builder = StringBuilder()
-
-        list.forEach { e -> builder.append(e.toString()).append(splitter) }
-
-        return builder.toString()
-    }
-
     fun search(query: String): SearchListResponse {
         var search = youtube.search().list("id,snippet")
 
@@ -77,6 +72,41 @@ class YoutubeBot(val key: String, val youtubeKey: String) {
     }
 
     fun downloadVideo(id: String): YoutubeVideo {
+        return executor.submit(VideoCallable(id)).get()
+    }
+
+    fun downloadPlaylist(options: PlaylistOptions, id: String): YoutubePlaylist {
+        return executor.submit(PlaylistCallable(options, id)).get()
+    }
+
+    fun parse8601Duration(i: String): Long {
+        var parts = i.split("T")
+        var days = 0
+
+        if (!"P".equals(parts[0])) {
+            days = Period.parse(parts[0]).days
+        }
+
+        var indices = arrayOf(arrayOf("H", 3600), arrayOf("M", 60), arrayOf("S", 1))
+        var parse = parts[1]
+        var seconds = 0L
+
+        for (ind in indices) {
+            var position = parse.indexOf(ind[0].toString())
+
+            if (position != -1) {
+                var value = parse.substring(0, position)
+                seconds += value.toInt() * ind[1].toString().toInt()
+                parse = parse.substring(value.length + 1)
+            }
+        }
+
+        return days * 86400 + seconds
+    }
+}
+
+class VideoCallable(val id: String): Callable<YoutubeVideo> {
+    override fun call(): YoutubeVideo {
         println("Downloading $id...")
         ProcessBuilder().command("./youtube-dl", "--write-info-json",
                 "--id", "-x", "--audio-format", "mp3", "--audio-quality", "0",
@@ -87,8 +117,10 @@ class YoutubeBot(val key: String, val youtubeKey: String) {
 
         return YoutubeVideo(id, File("$id.mp3")).fetchMetadata()
     }
+}
 
-    fun downloadPlaylist(options: PlaylistOptions, id: String): YoutubePlaylist {
+class PlaylistCallable(val options: PlaylistOptions, val id: String): Callable<YoutubePlaylist> {
+    override fun call(): YoutubePlaylist {
         var folder = File(id)
 
         if (folder.exists()) {
@@ -127,29 +159,12 @@ class YoutubeBot(val key: String, val youtubeKey: String) {
         return playlist
     }
 
-    fun parse8601Duration(i: String): Long {
-        var parts = i.split("T")
-        var days = 0
+    private fun addSplit(list: List<Any>, splitter: String): String {
+        var builder = StringBuilder()
 
-        if (!"P".equals(parts[0])) {
-            days = Period.parse(parts[0]).days
-        }
+        list.forEach { e -> builder.append(e.toString()).append(splitter) }
 
-        var indices = arrayOf(arrayOf("H", 3600), arrayOf("M", 60), arrayOf("S", 1))
-        var parse = parts[1]
-        var seconds = 0L
-
-        for (ind in indices) {
-            var position = parse.indexOf(ind[0].toString())
-
-            if (position != -1) {
-                var value = parse.substring(0, position)
-                seconds += value.toInt() * ind[1].toString().toInt()
-                parse = parse.substring(value.length + 1)
-            }
-        }
-
-        return days * 86400 + seconds
+        return builder.toString()
     }
 }
 
