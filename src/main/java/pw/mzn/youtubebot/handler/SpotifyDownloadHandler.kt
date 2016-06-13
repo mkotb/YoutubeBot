65f468e718aa16882a6cd47cb8c85970e6f178be
Mazen Kotb <email@mazenmc.io>
2016-06-12 21:37:50 -0700

    or not

--- src/main/java/pw/mzn/youtubebot/handler/SpotifyDownloadHandler.kt
+++ src/main/java/pw/mzn/youtubebot/handler/SpotifyDownloadHandler.kt
@@ -25,40 +25,35 @@ class SpotifyDownloadHandler(val instance: YoutubeBot) {
                     var track = e.track
                     var name = track.name
                     var artist = track.artists[0].name
+                    var videos = instance.searchVideo("$artist $name")
+                    var mapped = videos.associateBy { e -> e.videoId }
+                    var search = youtube.videos().list("id,snippet")
 
-                    try {
-                        var videos = instance.searchVideo("$artist $name")
-                        var mapped = videos.associateBy { e -> e.videoId }
-                        var search = youtube.videos().list("id,snippet")
+                    search.id = videos.map { e -> e.videoId }.joinToString(",")
+                    search.fields = "items(id,snippet/channelTitle)"
+                    search.key = instance.youtubeKey
 
-                        search.id = videos.map { e -> e.videoId }.joinToString(",")
-                        search.fields = "items(id,snippet/channelTitle)"
-                        search.key = instance.youtubeKey
+                    videos = search.execute().items
+                            .associate { e -> Pair(e.snippet.channelTitle, mapped[e.id]) }
+                            .filter { e -> !e.key.toLowerCase().contains("vevo") &&
+                                    !e.value!!.title.toLowerCase().contains("music video") }
+                            .values.map { e -> e!! }
+                            .toList()
 
-                        videos = search.execute().items
-                                .associate { e -> Pair(e.snippet.channelTitle, mapped[e.id]) }
-                                .filter { e -> !e.key.toLowerCase().contains("vevo") &&
-                                        !e.value!!.title.toLowerCase().contains("music video") }
-                                .values.map { e -> e!! }
-                                .toList()
-
-                        if (videos.isEmpty()) {
-                            current.chat.sendMessage("Could not find a suitable video for $name by $artist")
-                            return@run
-                        }
-
-                        var cover = track.album.images[0].url
-                        var options = VideoOptions(0, 0, false, 1.0, !"".equals(cover), cover, artist, name)
-                        var video = instance.downloadVideo(options, videos[0].videoId)
-
-                        video.customPerformer = artist
-                        video.customTitle = name
-
-                        instance.command.video.sendProcessedVideo(video, null, current.chat, current.chat.id.toLong(), true,
-                                options, null)
-                    } catch (e: Exception) {
-                        current.chat.sendMessage("Could not download $name due to an exception, please contact @MazenK")
+                    if (videos.isEmpty()) {
+                        current.chat.sendMessage("Could not find a suitable video for $name by $artist")
+                        return@run
                     }
+
+                    var cover = track.album.images[0].url
+                    var options = VideoOptions(0, 0, false, 1.0, !"".equals(cover), cover, artist, name)
+                    var video = instance.downloadVideo(options, videos[0].videoId)
+
+                    video.customPerformer = artist
+                    video.customTitle = name
+
+                    instance.command.video.sendProcessedVideo(video, null, current.chat, current.chat.id.toLong(), true,
+                            options, null)
                 } }
 
                 queue.poll()
